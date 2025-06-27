67dbc17e05b645e27de48aab93b3a0399581cf85
Srinivas917 <91753394+Srinivas917@users.noreply.github.com>
2025-06-27 11:55:59 +0530

    app.py

--- app.py
+++ app.py
@@ -5,6 +5,6 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return f"Hello, {os.getenv('NAME','WORLD')}"
+    return f"Hello, {os.getenv('NAME','WORLD')}.<br>{os.getenv('NAME','WORLD')}, Everything is working fine. You can go on..."
 if __name__ == '__main__':
     app.run(host = '0.0.0.0',port = 80)
